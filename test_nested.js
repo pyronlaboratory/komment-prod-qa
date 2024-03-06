a4cf328fdb1700ca2a43b25f285caefeae5540e6
Ronnie <ronnie94official@gmail.com>
2024-03-06 11:23:38 +0000

    Create test_nested.js

---
+++ test_nested.js
@@ -0,0 +1,33 @@
+/**
+ * @description defines a variable `self` that increments its age by 1 every 1000
+ * milliseconds through a setInterval callback function.
+ */
+function Person() {
+  const self = this;
+  self.age = 0;
+
+  setInterval(function growUp() {
+    self.age++;
+  }, 1000);
+}
+
+
+const getCode = (function () {
+  const apiCode = "0]Eal(eh&2";
+
+  return function () {
+    return apiCode;
+  };
+})();
+
+console.log(getCode());
+
+const pet = function (name) {
+  const getName = function () {
+    return name;
+  };
+  return getName;
+};
+const myPet = pet("Vivie");
+
+console.log(myPet());
